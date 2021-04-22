dfbcabf2e88c4396582efea6061aaa5828e88c17
Lakshman <lakshmansakhileti@gmail.com>
2021-04-22 06:25:05 +0530

    Initial spring boot setup

---
+++ src/main/java/com/pramati/imaginea/ImagineaApplication.java
@@ -0,0 +1,13 @@
+package com.pramati.imaginea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagineaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagineaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pramati/imaginea/ImagineaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pramati.imaginea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagineaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
